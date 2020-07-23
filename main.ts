966679fd21a83bbec0b2a1bbdc6c4a8229f93996
celestequinn <68699861+celestequinn@users.noreply.github.com>
2020-07-23 11:55:20 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showArrow(ArrowNames.East)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showArrow(ArrowNames.West)
+})
